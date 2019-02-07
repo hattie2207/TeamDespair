c3f0001823d99664a8282b50a98904cd76e91ca5
Hattie Samankul <hattie.samankul@hotmail.com>
2019-02-07 18:19:54 +0000

    get data and populate options

--- Develop/get.js
+++ Develop/get.js
@@ -1,19 +1,57 @@
 const requestURL = "http://localhost:9966/petclinic/api/";
 
-function getData(attribute) {
-    "use strict";  
+function getData(attribute, onload) {
+    "use strict";
 
     var request = new XMLHttpRequest();
     request.open("GET", requestURL + attribute);
     request.responseType = "json";
     request.send();
-    request.onload = function () {
 
-        var result = request.response;
-        let resultStr = JSON.stringify(result);
-        // document.body.innerHTML += resultStr;
+    request.onload = () => onload(request.response);
+}
 
+function populatePopup(attribute) {
+    function onDataLoaded(result) {
+        deletePopup();
+        let counter = 0;
+        for (let name of result) {
+
+            elementCreate("option", counter, "" + name.firstName, "options");
+            counter++;
+        }
+    }
+    getData(attribute, onDataLoaded);
+
+}
+
+function deletePopup() {
+    deleteElementList("options");
+}
+
+function deleteElementList(className) {
+    let list = document.getElementsByClassName(className);
+    let length = list.length;
+    for (let i = 0; i < length; i++) {
+        list[0].remove();
     }
 }
 
-getData("owners");
+function elementCreate(tagName, id, value, className) {
+
+    let element = document.createElement(tagName);
+    element.className = className;
+    element.id = id;
+    element.value = value;
+    element.textContent = value;
+    document.getElementById("selectedValue").appendChild(element);
+}
+
+function confirmChoice() {
+    let attr = document.getElementById("selectedKey");
+    populatePopup(attr.value);
+}
+
+function viewData() {
+
+}

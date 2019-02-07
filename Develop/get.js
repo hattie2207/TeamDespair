49c66600dd9ceb51c5adcb27860bdeddfa552ffb
Hattie Samankul <hattie.samankul@hotmail.com>
2019-02-07 15:46:17 +0000

    in progress: making interface to select which data to get and display

--- Develop/get.js
+++ Develop/get.js
@@ -1,17 +1,19 @@
+const requestURL = "http://localhost:9966/petclinic/api/";
 
-"use strict";
-var requestURL = "http://localhost:9966/petclinic/api/owners";
+function getData(attribute) {
+    "use strict";  
 
-var request = new XMLHttpRequest();
-request.open("GET", requestURL);
-request.responseType = "json";
-request.send();
-request.onload = function () {
+    var request = new XMLHttpRequest();
+    request.open("GET", requestURL + attribute);
+    request.responseType = "json";
+    request.send();
+    request.onload = function () {
 
-    var result = request.response;
-    let resultStr = JSON.stringify(result);
-    document.body.innerHTML += resultStr;
+        var result = request.response;
+        let resultStr = JSON.stringify(result);
+        // document.body.innerHTML += resultStr;
 
+    }
 }
 
-
+getData("owners");
